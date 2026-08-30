89a0ae7997ccdfcdfae7575b18df44e0f8788ebd
steleus <zhvarol16@gmail.com>
2026-08-30 23:29:24 +0300

    İzleme listesine film ekleme ve çıkarma özelliği eklendi.

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,18 +1,34 @@
 import type { Movie } from '../types/Movie';
+import {useWatchlist} from "../context/WatchlistContext";
+
 
 interface MovieCardProps {
     movie: Movie;
 }
 
 function MovieCard({ movie }: MovieCardProps) {
+    const { addToWatchlist, removeFromWatchlist, isInWatchlist } = useWatchlist();
+
+    const inWatchlist = isInWatchlist(movie.id);
+
+
+
+
     return (
         <div>
-            <img src={movie.poster} alt={movie.title} />
+            <img src={movie.poster ?? ""} alt={movie.title} />
             <h2>{movie.title}</h2>
             <p>{movie.year}</p>
             <p>{movie.type === 'movie' ? 'Film' : 'Dizi'}</p>
             <p>⭐ {movie.rating}</p>
             <p>{movie.description}</p>
+
+            <button onClick={() => inWatchlist ? removeFromWatchlist(movie.id) : addToWatchlist(movie)}>
+                {inWatchlist ? 'Listeden Kaldır' : 'İzleme Listeme Ekle'}
+            </button>
+
+
+
 
         </div>
     );
