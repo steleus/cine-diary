797e788f6641271f76a026fea2d0b9ea6854ebdc
steleus <zhvarol16@gmail.com>
2026-08-31 18:06:07 +0300

    Projenin son düzenlemeleri yapıldı.

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,10 +1,10 @@
-import Lottie from "lottie-react";
+import { Lottie } from "lottie-react";
 import spinner from "../assets/spinner.json";
 
 function Spinner() {
   return (
     <div className="flex justify-center items-center py-8">
-      <Lottie animationData={spinner} loop={true} />
+      <Lottie src={spinner} autoplay loop />
     </div>
   );
 }
